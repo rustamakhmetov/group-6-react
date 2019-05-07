1497f2050abb1e28e3f9fd78a4bed80d9e1e5436
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2019-05-07 17:34:51 +0500

    [task1] math class

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+import math from '~/src/lib';
+
+console.log('Math:');
+console.log('1 + 2 = ', math.add(1, 2));
+console.log('1 - 2 = ', math.sub(1, 2));
+console.log('1 * 2 = ', math.mul(1, 2));
+console.log('1 / 2 = ', math.div(1, 2));
